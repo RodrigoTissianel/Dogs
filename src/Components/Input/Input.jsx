059a5653e025c14e34dc6bb87cf7eb50da1e94fc
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-05 02:31:29 -0300

    feat: create the Input component

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,5 +1,23 @@
-const Input = () => {
-    return <div>Input</div>;
+import Error from '../Helper/Error/Error';
+import styles from './Input.module.css';
+
+const Input = ({ label, type, name, value, onChange, onBlur, error }) => {
+    return (
+        <div className={styles.wrapper}>
+            <label htmlFor={name} className={styles.label}>
+                {label}
+            </label>
+            <input
+                type={type}
+                id={name}
+                className={styles.input}
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+            />
+            {error && <Error error={error} />}
+        </div>
+    );
 };
 
 export default Input;
