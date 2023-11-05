af478119586ea0c1d9f01df7fdf585df4af750f9
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-05 17:54:48 -0300

    feat: create the Button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,5 +1,11 @@
-const Button = () => {
-    return <div>Button</div>;
+import styles from './Button.module.css';
+
+const Button = ({ children, ...props }) => {
+    return (
+        <button className={styles.button} {...props}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
