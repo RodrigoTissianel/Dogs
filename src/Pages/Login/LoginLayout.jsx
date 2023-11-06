59257e56be5410dd5f072fc4b2a3079e8de2d520
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-05 22:10:53 -0300

    feat(LoginLayout): add the navigation to account automatically when logged in

--- src/Pages/Login/LoginLayout.jsx
+++ src/Pages/Login/LoginLayout.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { UserContext } from '../../Context/UserContext';
+import styles from './LoginLayout.module.css';
 
 const LoginLayout = () => {
+    const { login } = React.useContext(UserContext);
+    if (login) return <Navigate to="/account" />;
+
     return (
-        <section>
-            <div>
+        <section className={styles.login}>
+            <div className={styles.forms}>
                 <Outlet />
             </div>
         </section>
