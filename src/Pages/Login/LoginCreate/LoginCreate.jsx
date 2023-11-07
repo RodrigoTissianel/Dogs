bed7e018dbfa48e96c15e7b3023abbe5ee75b81e
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-06 21:53:46 -0300

    feat: create the LoginCreate component

--- src/Pages/Login/LoginCreate/LoginCreate.jsx
+++ src/Pages/Login/LoginCreate/LoginCreate.jsx
@@ -1,5 +1,57 @@
+import React from 'react';
+import { USER_POST } from '../../../Api';
+import Button from '../../../Components/Button/Button';
+import Error from '../../../Components/Helper/Error/Error';
+import Input from '../../../Components/Input/Input';
+import { UserContext } from '../../../Context/UserContext';
+import useFetch from '../../../Hooks/useFetch';
+import useForm from '../../../Hooks/useForm';
+
 const LoginCreate = () => {
-    return <div>LoginCreate</div>;
+    const username = useForm();
+    const email = useForm('email');
+    const password = useForm('password');
+    const { loading, error, request } = useFetch();
+    const { userLogin } = React.useContext(UserContext);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const { url, options } = USER_POST({
+            username: username.value,
+            email: email.value,
+            password: password.value,
+        });
+        const { response } = await request(url, options);
+        if (response.ok) userLogin(username.value, password.value);
+    }
+
+    return (
+        <section className="animeLeft">
+            <h1 className="title">Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    label="Usuário"
+                    type="text"
+                    name="username"
+                    {...username}
+                />
+                <Input label="Email" type="email" name="email" {...email} />
+                <Input
+                    label="Senha"
+                    type="password"
+                    name="password"
+                    {...password}
+                />
+                {loading ? (
+                    <Button disabled>Cadastrando...</Button>
+                ) : (
+                    <Button>Cadastrar</Button>
+                )}
+
+                <Error error={error} />
+            </form>
+        </section>
+    );
 };
 
 export default LoginCreate;
