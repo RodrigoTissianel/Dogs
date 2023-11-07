9d1bc3d313ed8eff879d97f7cd665466171b2d73
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-06 21:26:57 -0300

    feat: create the header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,36 @@
+import React from 'react';
+import Dogs from '../../assets/dogs.svg?react';
+import { UserContext } from '../../Context/UserContext';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
 const Header = () => {
-    return <div>Header</div>;
+    const { data, userLogout } = React.useContext(UserContext);
+
+    return (
+        <header className={styles.header}>
+            <nav className={`${styles.nav} container`}>
+                <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+                    <Dogs />
+                </Link>
+
+                {data ? (
+                    <div>
+                        <Link to="/account" className={styles.login}>
+                            {data.nome}
+                        </Link>
+                        <button onClick={userLogout}>Sair</button>
+                    </div>
+                ) : (
+                    <div>
+                        <Link to="/login" className={styles.login}>
+                            Login / Criar
+                        </Link>
+                    </div>
+                )}
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
