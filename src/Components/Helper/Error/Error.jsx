d4d428fbfdac56650910c8ca43580d395c0af94a
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-06 21:25:54 -0300

    feat: create the Error component

--- src/Components/Helper/Error/Error.jsx
+++ src/Components/Helper/Error/Error.jsx
@@ -1,5 +1,7 @@
-const Error = () => {
-    return <div>Error</div>;
+const Error = ({ error }) => {
+    if (!error) return null;
+
+    return <p style={{ color: '#f31', margin: '1rem 0' }}>{error}</p>;
 };
 
 export default Error;
