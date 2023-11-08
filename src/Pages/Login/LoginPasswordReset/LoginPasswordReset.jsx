a886abdcfcd28030898721c2f765752da81ad4e5
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-07 21:59:38 -0300

    feat: create the LoginPasswordReset component

--- src/Pages/Login/LoginPasswordReset/LoginPasswordReset.jsx
+++ src/Pages/Login/LoginPasswordReset/LoginPasswordReset.jsx
@@ -1,5 +1,65 @@
+import React from 'react';
+import Input from '../../../Components/Input/Input';
+import useForm from '../../../Hooks/useForm';
+import useFetch from '../../../Hooks/useFetch';
+import Button from '../../../Components/Button/Button';
+import Error from '../../../Components/Helper/Error/Error';
+import { PASSWORD_RESET } from '../../../Api';
+import { useNavigate } from 'react-router-dom';
+
 const LoginPasswordReset = () => {
-    return <div>LoginPasswordReset</div>;
+    const [login, setLogin] = React.useState('');
+    const [key, setKey] = React.useState('');
+    const { loading, error, request } = useFetch();
+    const password = useForm('password');
+    const navigate = useNavigate();
+
+    React.useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const key = params.get('key');
+        const login = params.get('login');
+
+        if (key) setKey(key);
+        if (login) setLogin(login);
+    }, []);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        if (password.validate()) {
+            const { url, options } = PASSWORD_RESET({
+                login,
+                key,
+                password: password.value,
+            });
+
+            const { response } = await request(url, options);
+            if (response.ok) navigate('/login');
+        }
+    }
+
+    return (
+        <section className="animeLeft">
+            <h1 className="title">Resete sua senha</h1>
+
+            <form onSubmit={handleSubmit}>
+                <Input
+                    label="Nova senha:"
+                    type="password"
+                    name="password"
+                    {...password}
+                />
+
+                {loading ? (
+                    <Button disabled>Resetando...</Button>
+                ) : (
+                    <Button>Resetar senha</Button>
+                )}
+
+                <Error error={error} />
+            </form>
+        </section>
+    );
 };
 
 export default LoginPasswordReset;
