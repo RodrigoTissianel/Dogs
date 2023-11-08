cd359d9294c9d5e85662af57a006ca1b67157708
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-07 21:59:07 -0300

    feat: create the LoginPasswordLost component

--- src/Pages/Login/LoginPasswordLost/LoginPasswordLost.jsx
+++ src/Pages/Login/LoginPasswordLost/LoginPasswordLost.jsx
@@ -1,5 +1,49 @@
+import { PASSWORD_LOST } from '../../../Api';
+import Button from '../../../Components/Button/Button';
+import Error from '../../../Components/Helper/Error/Error';
+import Input from '../../../Components/Input/Input';
+import useFetch from '../../../Hooks/useFetch';
+import useForm from '../../../Hooks/useForm';
+
 const LoginPasswordLost = () => {
-    return <div>LoginPasswordLost</div>;
+    const login = useForm();
+    const { data, loading, error, request } = useFetch();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (login.validate()) {
+            const { url, options } = PASSWORD_LOST({
+                login: login.value,
+                url: window.location.href.replace('lost', 'reset'),
+            });
+            const { json } = await request(url, options);
+            console.log(json);
+        }
+    }
+    return (
+        <section className="animeLeft">
+            <h1 className="title">Perdeu a senha?</h1>
+            {data ? (
+                <p style={{ color: '#4c1' }}>{data}</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        label="Usuário / Email"
+                        type="text"
+                        name="login"
+                        {...login}
+                    />
+                    {loading ? (
+                        <Button disabled>Enviando...</Button>
+                    ) : (
+                        <Button>Enviar</Button>
+                    )}
+
+                    <Error error={error} />
+                </form>
+            )}
+        </section>
+    );
 };
 
 export default LoginPasswordLost;
