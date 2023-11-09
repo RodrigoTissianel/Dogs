dc24b77906c15116a7b4a32e4f397f4141a37e42
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-09 16:00:52 -0300

    feat: Create the UserHeader component

--- src/Pages/User/UserHeader/UserHeader.jsx
+++ src/Pages/User/UserHeader/UserHeader.jsx
@@ -1,5 +1,33 @@
+import React from 'react';
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router-dom';
+import UserHeaderNav from '../UserHeaderNav/UserHeaderNav';
+
 const UserHeader = () => {
-    return <div>UserHeader</div>;
+    const [title, setTitle] = React.useState('');
+    const location = useLocation();
+
+    React.useEffect(() => {
+        const { pathname } = location;
+
+        switch (pathname) {
+            case '/account/post':
+                setTitle('Poste sua foto');
+                break;
+            case '/account/statistics':
+                setTitle('Estatísticas');
+                break;
+            default:
+                setTitle('Minha conta');
+        }
+    }, [location]);
+
+    return (
+        <header className={styles.header}>
+            <h1 className="title">{title}</h1>
+            <UserHeaderNav />
+        </header>
+    );
 };
 
 export default UserHeader;
