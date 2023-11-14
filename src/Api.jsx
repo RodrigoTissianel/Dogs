8cb8a8c47a80973e54efb1c8f498ec6ae7575e45
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-14 16:13:02 -0300

    feat(Api): add the PHOTO_POST function

--- src/Api.jsx
+++ src/Api.jsx
@@ -75,3 +75,16 @@ export function PASSWORD_RESET(body) {
         },
     };
 }
+
+export function PHOTO_POST(formData, token) {
+    return {
+        url: API_URL + '/api/photo',
+        options: {
+            method: 'POST',
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+            body: formData,
+        },
+    };
+}
