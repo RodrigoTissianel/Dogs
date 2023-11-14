a0b14525f196cad1cde1021696570221a406a91b
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-14 16:08:28 -0300

    refactor(UserLayout): remove the empty div

--- src/Pages/User/UserLayout.jsx
+++ src/Pages/User/UserLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import ProtectedRoute from '../../Components/Helper/ProtectedRoute/ProtectedRoute';
+import UserHeader from './UserHeader/UserHeader';
 
 const UserLayout = () => {
     return (
         <ProtectedRoute>
             <section className="container">
-                <div>
-                    <Outlet />
-                </div>
+                <UserHeader />
+                <Outlet />
             </section>
         </ProtectedRoute>
     );
