cf89dce66c5f99fcb1f00325de26ee2eebb83d02
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-14 16:10:04 -0300

    feat: create the UserHeaderNav component

--- src/Pages/User/UserHeaderNav/UserHeaderNav.jsx
+++ src/Pages/User/UserHeaderNav/UserHeaderNav.jsx
@@ -1,5 +1,65 @@
+import React from 'react';
+import useMedia from '../../../Hooks/useMedia';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import MyPhotos from '../../../assets/feed.svg?react';
+import Statisctics from '../../../assets/estatisticas.svg?react';
+import AddPhoto from '../../../assets/adicionar.svg?react';
+import LogOut from '../../../assets/sair.svg?react';
+import styles from './UserHeaderNav.module.css';
+import { UserContext } from '../../../Context/UserContext';
+
 const UserHeaderNav = () => {
-    return <div>UserHeaderNav</div>;
+    const { userLogout } = React.useContext(UserContext);
+    const [mobileMenu, setMobileMenu] = React.useState(false);
+    const mobile = useMedia('(max-width: 40rem');
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    React.useEffect(() => {
+        setMobileMenu(false);
+    }, [pathname]);
+
+    function handleLogout() {
+        userLogout();
+        navigate('/login');
+    }
+
+    return (
+        <>
+            {mobile && (
+                <button
+                    aria-label="Menu"
+                    className={`${styles.mobileButton} 
+                    ${mobileMenu && styles.mobileButtonActive}`}
+                    onClick={() => setMobileMenu(!mobileMenu)}
+                ></button>
+            )}
+            <nav
+                className={`${mobile ? styles.navMobile : styles.nav} 
+            ${mobileMenu && styles.navMobileActive}`}
+            >
+                <NavLink to="/account" end>
+                    <MyPhotos />
+                    {mobile && 'Minhas fotos'}
+                </NavLink>
+
+                <NavLink to="/account/statistics">
+                    <Statisctics />
+                    {mobile && 'Estatísticas'}
+                </NavLink>
+
+                <NavLink to="/account/post">
+                    <AddPhoto />
+                    {mobile && 'Adicionar foto'}
+                </NavLink>
+
+                <button onClick={handleLogout}>
+                    <LogOut />
+                    {mobile && 'Sair'}
+                </button>
+            </nav>
+        </>
+    );
 };
 
 export default UserHeaderNav;
