b94c31a09f905d823df7b654979e21dbc43a581e
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-27 22:17:50 -0300

    feat: create the PhotoContent component

---
+++ src/Pages/Photo/PhotoContent/PhotoContent.jsx
@@ -0,0 +1,50 @@
+// import React from 'react';
+import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
+import PhotoComments from '../PhotoComments/PhotoComments';
+import { useContext } from 'react';
+import { UserContext } from '../../../Context/UserContext';
+import PhotoDelete from '../PhotoDelete/PhotoDelete';
+
+const PhotoContent = ({ data }) => {
+    const { comments, photo } = data;
+    const user = useContext(UserContext);
+    console.log(user.data);
+    return (
+        <div className={styles.photo}>
+            <div className={styles.img}>
+                <img src={photo.src} alt={photo.title} />
+            </div>
+
+            <div className={styles.details}>
+                <div>
+                    <p className={styles.author}>
+                        {user.data.username === photo.author ? (
+                            <PhotoDelete id={photo.id} />
+                        ) : (
+                            <Link
+                                to={`/profile/${photo.author}`}
+                            >{`@${photo.author}`}</Link>
+                        )}
+
+                        <span className={styles.visualizacoes}>
+                            {photo.acessos}
+                        </span>
+                    </p>
+
+                    <h1 className="title">
+                        <Link to={`photo/${photo.id}`}>{photo.title}</Link>
+                    </h1>
+
+                    <ul className={styles.attributes}>
+                        <li>{photo.peso} kg</li>
+                        <li>{photo.idade} anos</li>
+                    </ul>
+                </div>
+            </div>
+            <PhotoComments id={photo.id} comments={comments} />
+        </div>
+    );
+};
+
+export default PhotoContent;
