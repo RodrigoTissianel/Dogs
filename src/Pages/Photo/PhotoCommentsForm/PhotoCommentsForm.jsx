2232e2b472b2f8690e455a7b56da5da04e7d4894
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-27 22:30:40 -0300

    feat: create the PhotoCommentsForm component

---
+++ src/Pages/Photo/PhotoCommentsForm/PhotoCommentsForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './PhotoCommentsForm.module.css';
+import SendBtn from '../../../assets/enviar.svg?react';
+import { COMMENT_POST } from '../../../Api';
+import useFetch from '../../../Hooks/useFetch';
+import Error from '../../../Components/Helper/Error/Error';
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+    const { error, request } = useFetch();
+    const [comment, setComment] = React.useState('');
+
+    async function handleSubmit(event) {
+        const token = window.localStorage.getItem('token');
+        event.preventDefault();
+        const { url, options } = COMMENT_POST(id, { comment }, token);
+        const { response, json } = await request(url, options);
+        if (response.ok) {
+            setComment('');
+            setComments((comments) => [...comments, json]);
+        }
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <textarea
+                className={styles.textarea}
+                name="comment"
+                id="comment"
+                placeholder="Escreva um comentário..."
+                value={comment}
+                onChange={({ target }) => setComment(target.value)}
+            />
+            <button className={styles.button}>
+                <SendBtn />
+            </button>
+            <Error error={error} />
+        </form>
+    );
+};
+
+export default PhotoCommentsForm;
