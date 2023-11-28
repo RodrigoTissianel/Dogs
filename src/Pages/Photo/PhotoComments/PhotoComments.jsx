5fc86e054f83add8104aff2ab7c7205f9115065e
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2023-11-27 22:23:20 -0300

    feat: create the PhotoComment component

---
+++ src/Pages/Photo/PhotoComments/PhotoComments.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useRef } from 'react';
+import { UserContext } from '../../../Context/UserContext';
+import PhotoCommentsForm from '../PhotoCommentsForm/PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
+
+const PhotoComments = (props) => {
+    const [comments, setComments] = React.useState(() => props.comments);
+    const { login } = useContext(UserContext);
+    const commentsSection = useRef(null);
+
+    React.useEffect(() => {
+        commentsSection.current.scrollTop =
+            commentsSection.current.scrollHeight;
+    }, [comments]);
+
+    return (
+        <>
+            <ul ref={commentsSection} className={styles.comments}>
+                {comments.map((comment) => (
+                    <li key={comment.comment_ID}>
+                        <b>{comment.comment_author}: </b>
+                        <span>{comment.comment_content}</span>
+                    </li>
+                ))}
+            </ul>
+            {login && (
+                <PhotoCommentsForm id={props.id} setComments={setComments} />
+            )}
+        </>
+    );
+};
+
+export default PhotoComments;
