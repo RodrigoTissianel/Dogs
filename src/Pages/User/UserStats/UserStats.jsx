c3d816b5c65461b96028c8fd12733707a8b19559
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2024-01-11 21:41:49 -0300

    feat: add the user stats page

--- src/Pages/User/UserStats/UserStats.jsx
+++ src/Pages/User/UserStats/UserStats.jsx
@@ -1,12 +1,35 @@
+import React from 'react';
+import { STATS_GET } from '../../../Api';
+import useFetch from '../../../Hooks/useFetch';
+import Loading from '../../../Components/Helper/Loading/Loading';
 import Head from '../../../Components/Helper/Head/Head';
+import Error from '../../../Components/Helper/Error/Error';
+
+const UserStatsGraphs = React.lazy(() =>
+    import('./UserStatsGraphs/UserStatsGraphs')
+);
 
 const UserStats = () => {
-    return (
-        <div>
-            <Head title="Estatísticas" />
-            UserStats
-        </div>
-    );
+    const { data, error, loading, request } = useFetch();
+
+    React.useEffect(() => {
+        async function getData() {
+            const { url, options } = STATS_GET();
+            await request(url, options);
+        }
+        getData();
+    }, [request]);
+
+    if (loading) return <Loading />;
+    if (error) return <Error error={error} />;
+    if (data)
+        return (
+            <React.Suspense fallback={<div></div>}>
+                <Head title="Estatísticas" />
+                <UserStatsGraphs data={data} />
+            </React.Suspense>
+        );
+    else return null;
 };
 
 export default UserStats;
